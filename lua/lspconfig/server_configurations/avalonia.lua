5653ae4148497425370010b7e6ef39956876fdbc
seasea128 <seasea@windowslive.com>
2024-08-27 00:57:18 +0700

    fix: default path for AvaloniaUI

--- lua/lspconfig/server_configurations/avalonia.lua
+++ lua/lspconfig/server_configurations/avalonia.lua
@@ -2,7 +2,7 @@ local util = require 'lspconfig.util'
 
 return {
   default_config = {
-    cmd = { 'dotnet', 'AvaloniaLanguageServer.dll' },
+    -- cmd = { 'dotnet', 'AvaloniaLanguageServer.dll' },
     filetype = { 'axaml' },
     root_dir = util.root_pattern('*.sln', '*.csproj'),
   },
